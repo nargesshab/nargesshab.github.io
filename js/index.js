3b60364f397068a2d03bc8d068d354a4edc0cf03
nargesshab <46818250+nargesshab@users.noreply.github.com>
2019-05-01 15:17:58 +0200

    adding java
    changing the java and pictires

---
+++ js/index.js
@@ -0,0 +1,11 @@
+(function () {
+  let element = document.querySelector('.scroll-horizontal-container')
+  element.addEventListener('wheel', (e) => {
+    if (e.deltaY !== 0) {
+      e.preventDefault()
+      window.requestAnimationFrame(() => {
+        element.scrollLeft += e.deltaY
+      })
+    }
+  })
+})()
