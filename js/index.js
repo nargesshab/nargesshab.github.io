9c0aa7b5ec1abf73e76459c5f3619c4383c4ca8f
Nargess <shabani@mail.scuoladesign.com>
2019-05-17 10:15:17 +0200

    new edit
    correction of accordion and some others

--- js/index.js
+++ js/index.js
@@ -9,3 +9,37 @@
     }
   })
 })()
+
+
+
+var tween = TweenMax.to('.box', .5, { 
+  width: '+=800',
+  height:'100%'
+});
+
+var tween1 = TweenMax.to(".box1", 0.5, {
+  width: '+=800'
+});
+
+
+
+var Controller = new ScrollMagic.Controller({
+  vertical: false });
+
+
+var Scene = new ScrollMagic.Scene({
+  triggerElement: '.box',
+  duration: 600 });
+
+Scene.setTween(tween).
+addIndicators().
+addTo(Controller);
+
+var Scene1 = new ScrollMagic.Scene({
+  triggerElement: '.box1',
+  duration: 600 });
+
+Scene1.setTween(tween1).
+addIndicators().
+addTo(Controller);
+
